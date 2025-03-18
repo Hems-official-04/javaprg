423bf46c18f097ffe2461294effe2f921ac98ae3
Hems-official-04 <meetathemambbs@gmail.com>
2025-03-18 14:55:24 +0530

    Add files via upload

---
+++ Addon2/src/Addon2/WrapperClass.java
@@ -0,0 +1,10 @@
+package Addon2;
+
+public class WrapperClass {
+	public static void main(String[]args) {
+		Integer intObj =50;//autoboxing
+		int intValue = intObj;
+		System.out.println(intValue);//unboxing
+	}
+
+}
